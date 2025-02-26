ffcc3fbd09d11948034baf9df361846c5779b745
지성배 <seongbae@eu-cast.com>
2025-02-26 08:16:17 +0000

    finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://seongbae-tutorial-astro.netlify.app/"
+  site: "https://seongbae-tutorial-astro.netlify.app/",
+  integrations: [preact()]
 });
