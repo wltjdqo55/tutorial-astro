a56dec4c80268bcdf04795b81ad4f0f8e1a4431f
지성배 <seongbae@eu-cast.com>
2025-02-27 01:25:14 +0000

    0227/1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,27 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+import { jsx } from 'preact/jsx-runtime';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import react from '@astrojs/react';
+
+import solidJs from '@astrojs/solid-js';
+
+import svelte from '@astrojs/svelte';
+
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://seongbae-tutorial-astro.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact(), alpinejs(), react(), solidJs(), svelte(), vue()],
+  base: '/',
+  trailingSlash : 'ignore',
+  redirects : {
+    '/hi': '/blog',
+    '/hello': '/about'
+  },
+  output : 'server' // SSR 방식
 });
